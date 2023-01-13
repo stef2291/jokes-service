794ae7fe9ea8d33c20162655aff6a8be52589a59
Stefanos Michaelides <stefanos2291@gmail.com>
2023-01-13 12:24:37 +0000

    all tests pass

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
-const express = require('express');
+const express = require("express");
+const { Op } = require("sequelize");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
+  let jokes = [];
   try {
+    if (Object.keys(req.query).length === 0) {
+      jokes = await Joke.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+      });
+      res.status(200).send(jokes);
+      return;
+    }
+    if (req.query.tags) {
+      console.log("");
+      if (Array.isArray(req.query.tags)) {
+        const arr = req.query.tags;
+
+        joinedArr = arr.join(",");
+
+        jokes = await Joke.findAll({
+          where: {
+            [Op.or]: [{ tags: arr[0] }, { tags: arr[1] }],
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        });
+        res.status(200).send(jokes);
+        return;
+      } else {
+        const str = req.query.tags;
+        jokes = await Joke.findAll({
+          where: {
+            tags: {
+              [Op.like]: "%" + req.query.tags + "%",
+            },
+          },
+          attributes: {
+            exclude: ["createdAt", "updatedAt"],
+          },
+        });
+        res.status(200).send(jokes);
+        return;
+      }
+    }
+
+    if (req.query.content) {
+      console.log(req.query);
+      jokes = await Joke.findAll({
+        where: {
+          joke: {
+            [Op.like]: "%" + req.query.content + "%",
+          },
+        },
+        attributes: {
+          exclude: ["createdAt", "updatedAt"],
+        },
+      });
+      res.status(200).send(jokes);
+    }
+
     // TODO - filter the jokes by tags and content
-    const jokes = [];
-    res.send(jokes);
   } catch (error) {
+    console.log(error);
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
---
+++ testing.js
@@ -0,0 +1,15 @@
+let produce = [
+  { name: "Carrot", color: "orange", type: "vegetable" },
+  { name: "Celery", color: "green", type: "vegetable" },
+  { name: "Eggplant", color: "purple", type: "vegetable" },
+  { name: "Cherry", color: "red", type: "fruit" },
+  { name: "Apple", color: "red", type: "fruit" },
+  { name: "Banana", color: "yellow", type: "fruit" },
+  { name: "Blueberry", color: "blue", type: "fruit" },
+  { name: "Grapefruit", color: "orange", type: "fruit" },
+  { name: "Kumquat", color: "green", type: "fruit" },
+];
+
+const keys = Object.keys(produce[0]);
+
+console.log(keys);
